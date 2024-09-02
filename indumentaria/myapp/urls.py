6f9555a0c30486d8a4f89feb908c142702ebed76
Tomás <tomasheinzmann09@mgmail.com>
2024-09-02 12:34:51 -0300

    Se agrego la funcion de guardar remeras

--- indumentaria/myapp/urls.py
+++ indumentaria/myapp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path("shop/", views.shop, name="shop"),
-    path("contacto/", views.contacto, name="contacto")
+    path("contacto/", views.contacto, name="contacto"),
+    path("nueva_remera/", views.nueva_remera, name="nueva_remera")
 ]
--- indumentaria/myapp/views.py
+++ indumentaria/myapp/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render, HttpResponse
-
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from . import forms
+import sqlite3
 def shop(request):
     return render(request, "myapp/shop.html")
 
 def contacto(request):
     return render(request, "myapp/contacto.html")
+
+def nueva_remera(request):
+    if request.method == "POST":
+        form = forms.FormularioRemera(request.POST)
+        if form.is_valid():
+            conn = sqlite3.connect("db.sqlite3")
+            cursor = conn.cursor()
+            cursor.execute("INSERT INTO remeras VALUES (?, ?, ?, ?, ?)" , (form.cleaned_data["marca"], form.cleaned_data["talle"], form.cleaned_data["color"], form.cleaned_data["lisa"], form.cleaned_data["genero"]) )
+            conn.commit()
+            conn.close()
+            return HttpResponseRedirect(reverse("shop"))
+    else:
+        form = forms.FormularioRemera()
+    ctx = {"form": form}
+    return render(request, "myapp/nueva_remera.html", ctx)
